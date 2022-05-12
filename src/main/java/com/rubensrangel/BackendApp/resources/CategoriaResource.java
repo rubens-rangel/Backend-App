e65fe3c1db0d6e49e2eca897a4005c489769323b
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-05-12 09:09:09 -0300

    Rest test

---
+++ src/main/java/com/rubensrangel/BackendApp/resources/CategoriaResource.java
@@ -0,0 +1,17 @@
+package com.rubensrangel.BackendApp.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoriaResource {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar(){
+        return "Rest working";
+    }
+
+}
