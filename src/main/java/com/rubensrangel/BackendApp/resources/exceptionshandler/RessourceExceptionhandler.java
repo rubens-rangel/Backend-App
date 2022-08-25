8fbaad11b3f2df3d3e488494f0237f2a6b9fe6a4
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-08-25 15:13:56 -0300

    Configuração inicial security

--- src/main/java/com/rubensrangel/BackendApp/resources/exceptionshandler/RessourceExceptionhandler.java
+++ src/main/java/com/rubensrangel/BackendApp/resources/exceptionshandler/RessourceExceptionhandler.java
@@ -15,24 +15,24 @@ import javax.servlet.http.HttpServletRequest;
 public class RessourceExceptionhandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 
-        StandartError err= new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
     @ExceptionHandler(DataIntegrityException.class)
-    public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+    public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 
         StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandartError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+    public ResponseEntity<StandartError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
 
         ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação", System.currentTimeMillis());
-        for (FieldError x : e.getBindingResult().getFieldErrors()){
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
             err.addError(x.getField(), x.getDefaultMessage());
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
