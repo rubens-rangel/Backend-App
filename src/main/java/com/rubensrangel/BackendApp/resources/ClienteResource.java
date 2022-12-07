c8b61c2dbc39734e4e373cece2a83198e79ba189
Rubens Rangel <rubens.rangel@b2wdigital.com>
2022-12-07 16:14:37 -0300

    autorizando endpoints para perfis especificos

--- src/main/java/com/rubensrangel/BackendApp/resources/ClienteResource.java
+++ src/main/java/com/rubensrangel/BackendApp/resources/ClienteResource.java
@@ -7,6 +7,7 @@ import com.rubensrangel.BackendApp.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +42,7 @@ public class ClienteResource {
         List<ClienteDTO> clienteDTOList = clienteList.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(clienteDTOList);
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
                                                        @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
@@ -70,7 +71,7 @@ public class ClienteResource {
         return ResponseEntity.noContent().build();
     }
 
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         clienteService.delete(id);
