ac8ea572a5a6639be302b96ad0803ffd13ba8281
Rubens Rangel <rubens.rangel@b2wdigital.com>
2022-12-16 09:44:23 -0300

    Salvando arquivos no S3

--- src/main/java/com/rubensrangel/BackendApp/BackendAppApplication.java
+++ src/main/java/com/rubensrangel/BackendApp/BackendAppApplication.java
@@ -4,6 +4,7 @@ import com.rubensrangel.BackendApp.domain.*;
 import com.rubensrangel.BackendApp.domain.enums.EstadoPagamento;
 import com.rubensrangel.BackendApp.domain.enums.TipoCliente;
 import com.rubensrangel.BackendApp.repositories.*;
+import com.rubensrangel.BackendApp.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +16,8 @@ import java.util.Arrays;
 @SpringBootApplication
 public class BackendAppApplication implements CommandLineRunner {
 
-
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendAppApplication.class, args);
@@ -23,7 +25,7 @@ public class BackendAppApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+    s3Service.uploadFile("//home/rubens//Área de Trabalho//pessoal//discord.jpeg");
 
     }
 
